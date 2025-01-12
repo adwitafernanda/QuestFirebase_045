d28f43dcebfe820ce80ec4a1112bae199dff8eef
adwitafernanda <adwitafernanda4@gmail.com>
2025-01-12 20:54:39 +0700

    Add - fungsi delete dalam homeview

--- app/src/main/java/com/example/questfirebase_045/ui/home/pages/HomeView.kt
+++ app/src/main/java/com/example/questfirebase_045/ui/home/pages/HomeView.kt
@@ -76,7 +76,7 @@ fun HomeScreen(
             modifier = Modifier.padding(innerPadding),
             onDetailClick = onDetailClick,
             onDeleteClick = {
-                viewModel.getMhs()
+                viewModel.deleteMhs(it)
             }
         )
     }
