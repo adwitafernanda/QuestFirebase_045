c20aea6dcabe11e24c04c252d7f7d7e9d516fdc7
adwitafernanda <adwitafernanda4@gmail.com>
2025-01-12 21:35:28 +0700

    ADD - fungsi PengelolaHalaman untuk mengatur navigasi halaman

--- app/src/main/java/com/example/questfirebase_045/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/questfirebase_045/ui/navigation/PengelolaHalaman.kt
@@ -1,2 +1,8 @@
 package com.example.questfirebase_045.ui.navigation
 
+import androidx.compose.ui.Modifier
+
+fun PengelolaHalaman(
+    modifier: Modifier,
+    navController:NavHostController = rememberNavController()
+){}
